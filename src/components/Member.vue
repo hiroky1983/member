8d7879f254ea93329bbe427d2a38f0d4157c214d
hiroky1983 <hirockysan1983@gmail.com>
2022-01-14 13:31:35 +0900

    メンバーリストの追加

--- src/components/Member.vue
+++ src/components/Member.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    <input
-      type="text"
-      placeholder="名前を入力"
-      class="input"
-      v-model="newMember"
-    />
-    <button>追加</button>
+    <div>
+      <input
+        type="text"
+        placeholder="名前を入力"
+        class="input"
+        v-model="newMember"
+      />
+      <button class="btn" @click="clickAdd">追加</button>
+    </div>
+    <div>
+      <ul>
+        <li v-for="member in members" v-bind:key="member">
+          {{ member }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -18,11 +27,30 @@ export default {
       newMember: "",
     };
   },
+  methods: {
+    clickAdd() {
+      this.members.push(this.newMember);
+      this.newMember = "";
+    },
+  },
 };
 </script>
 
 <style>
 .input {
-  padding: 4px;
+  padding: 6px;
+  background-color: white;
+  border-radius: 14px;
+}
+.btn {
+  margin-left: 4px;
+  background-color: black;
+  color: white;
+  padding: 6px;
+  border-radius: 14px;
+}
+.btn:hover {
+  background-color: gray;
+  cursor: pointer;
 }
 </style>
